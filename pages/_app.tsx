fa25cd26b0c0f69000e7670997f2ffcaf8f01694
Andrzej Marek <a.marek@fireup.pro>
2021-04-16 21:02:02 +0200

    added google analitic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,25 @@ import { appWithTranslation } from "next-i18next";
 import "../styles/styles.css";
 import "../styles/fontawesome-all.css";
 import "../styles/bootstrap.css";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+const isProduction = process.env.NODE_ENV === "production";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
